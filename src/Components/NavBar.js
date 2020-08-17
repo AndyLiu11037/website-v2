bcc1dc85cddd1aeac5b70f18594e63ec6fa6e0fc
Andy Liu <andyliu11037@gmail.com>
2020-08-17 22:54:52 +1000

    add animate

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,20 +1,21 @@
 import React, {Component} from 'react'
 import {Link, BrowserRouter as Router} from 'react-router-dom'
+import 'animate.css'
 import './../App.css'
 import './../fonts.css'
 class NavBar extends Component{
     render(){
         return(
-            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div className="website-name">Andy Liu's Resume</div>
+            <nav className="navbar navbar-expand-lg">
+                <div className="website-name animate__animated animate__bounce">Andy Liu's Resume</div>
                 <div className="navbar-nav ml-5">
                     <Router>
                         <a className="nav-item nav-link"> <Link to="/home"></Link> Home <span className="sr-only">(current)</span></a>
                         <a className="nav-item nav-link" href="/about">About</a>
                         <a className="nav-item nav-link" href="/projects">Past Projects</a>
                     </Router>
-
                 </div>
+
             </nav>
         )
 
